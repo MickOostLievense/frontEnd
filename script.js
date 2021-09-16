ae0a670bea1c04dfaaee6c2d2c692e50dfde2513
MickOostLievense <90262942+MickOostLievense@users.noreply.github.com>
2021-09-16 14:03:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var menuList = document.getElementById("menulist");
+
+menulist.style.maxHeight = "0px";
+
+function togglemenu(){
+
+        if(menulist.style.maxHeight == "0px")
+            {
+                menulist.style.maxHeight = "130px"
+            }
+        else
+            {
+                menulist.style.maxHeight = "0px"
+            }
+}
+console.log('test')
